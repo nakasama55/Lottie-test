90ad548e44acdd68474160f53c17bc721350dc13
NAKATOKI <68881787+nakasama55@users.noreply.github.com>
2024-05-09 13:33:06 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,13 +7,17 @@ lottie.loadAnimation({
   autoplay: true
 })
 
-lottie.loadAnimation({
-  container: document.getElementById('Toki'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: '/webdesign_hover.json'
+document.addEventListener("DOMContentLoaded", function(){
+  const animation = bodymovin.loadAnimation({
+   container: document.getElementById('Toki'), //index.htmlで指定したid
+   renderer: 'svg', // 描画形式
+   loop: true, // true→ループ/false→1回
+   autoplay: true, // 自動再生
+   path: 'webdesign_hover.json'
+ });
 });
+
+
 
 $(function () {
   // Lottieプレーヤーを取得
