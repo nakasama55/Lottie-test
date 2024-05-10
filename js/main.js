4edff41e1f6d30f13c301d6cd676f0c9db21d69e
NAKATOKI <68881787+nakasama55@users.noreply.github.com>
2024-05-10 14:55:18 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 lottie.loadAnimation({
-  container: document.getElementById('lottie'), // アニメーションをさせたいdivなどhtml要素ににつけた名前
+  container: document.getElementById('lottie01'), // アニメーションをさせたいdivなどhtml要素ににつけた名前
   renderer: 'svg',
   loop: true,
   autoplay: true,
   path: 'philosophy.json', // JSONファイルのパス
+});
+
+lottie.loadAnimation({
+  container: document.getElementById('lottie02'), // アニメーションをさせたいdivなどhtml要素ににつけた名前
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'anim/history.json', // JSONファイルのパス
 });
