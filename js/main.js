ebdc1ce2fec228af5147b97ec19346cb97326a18
NAKATOKI <68881787+nakasama55@users.noreply.github.com>
2024-05-10 16:05:59 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,3 +13,22 @@ lottie.loadAnimation({
   autoplay: true,
   path: 'anim/history.json', // JSONファイルのパス
 });
+
+
+$(function () {
+  // Lottieプレーヤーを取得
+  var lottiePlayer = document.getElementById('lottie');
+
+  // icon-block要素を取得
+  var iconBlock = document.getElementById('testoki');
+
+  // ホバー時にアニメーションを再生
+  testoki.addEventListener('mouseenter', function() {
+    lottie.play();
+  });
+
+  // ホバーが外れたらアニメーションを停止
+  testoki.addEventListener('mouseleave', function() {
+    lottie.stop();
+  });
+});
