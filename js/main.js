38cafc2888b16ec2a820d8669df80e57404bdca6
NAKATOKI <68881787+nakasama55@users.noreply.github.com>
2024-05-10 14:19:02 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,8 @@
-"use strict";
-
-// Lottieアニメーション1番目
 const container01 = document.getElementById("lottie01"); // htmlのidを定義
 lottie.loadAnimation({
   container: container01, // アニメーションを格納するDOM要素
   renderer: "svg", // svg、canvas、htmlの設定ができ、html側で生成されるタグが変わる
   loop: true, // アニメーションを繰り返し再生させるか
   autoplay: true, // 自動再生させるか
-  path: "/js/webdesign_hover.json", // JSONファイルのパス
-});
-
-// Lottieアニメーション2番目
-const container02 = document.getElementById("lottie02"); // htmlのidを定義
-lottie.loadAnimation({
-  container: container02, // アニメーションを格納するDOM要素
-  renderer: "svg", // svg、canvas、htmlの設定ができ、html側で生成されるタグが変わる
-  loop: true, // アニメーションを繰り返し再生させるか
-  autoplay: true, // 自動再生させるか
-  path: "/js/philosophy.json", // JSONファイルのパス
+  path: "webdesign_hover.json", // JSONファイルのパス
 });
